e09d279fa45db4ace61c2480482281853221e840
dasoya <66422476+dasoya@users.noreply.github.com>
2023-09-12 15:00:18 +0900

    0912 배열
    10808

---
+++ untitled/0X03 배열/10808.cpp
@@ -0,0 +1,20 @@
+//
+// Created by dasoya on 12.09.23.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int arr[26] = {0,};
+
+    for (int i = 0; i < s.length() ; ++i) {
+        arr[s[i]-'a']++;
+    }
+
+    for(int i : arr) cout << i << " ";
+
+}
