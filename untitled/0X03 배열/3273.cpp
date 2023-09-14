f8b6db3756f3f2894834cf56bf0144eeb3db3c39
dasoya <66422476+dasoya@users.noreply.github.com>
2023-09-14 10:58:25 +0900

    0914 배열 및 연결리스트

--- untitled/0X03 배열/3273.cpp
+++ untitled/0X03 배열/3273.cpp
@@ -17,13 +17,18 @@ int main(){
 
     cin >> x;
 
-    int cnt[1000001], result =0;
+    // cnt 안에는 x-i 가 들어가는데, x는 2백만 까지 나올 수 있는 인덱스 범위 예외처리 신경써야함.!
+    int cnt[2000001], result =0;
 
-    fill(cnt,cnt+1000001,0);
+    fill(cnt,cnt+2000001,0);
 
     for(int i : arr){
-        if(cnt[x-i]) {
-        //cout << i <<" ";
+
+        if(x <= i)
+            continue;
+
+        if(cnt[x-i] == 1) {
+       // cout << x-i << "| ";
         result++;}
         else cnt[i]++;
 
