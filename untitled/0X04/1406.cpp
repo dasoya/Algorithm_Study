577687ab748a2b9269fd4607ca2bd21c7cdd2de4
dasoya <66422476+dasoya@users.noreply.github.com>
2023-09-19 10:30:11 +0900

    0X04 연결 리스트

---
+++ untitled/0X04/1406.cpp
@@ -0,0 +1,53 @@
+//
+// Created by dasoya on 19.09.23.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    list<char> editor = {};
+
+    string s;
+    cin >> s;
+
+    for(auto i: s){
+        editor.push_back(i);
+    }
+
+    auto t = editor.end();
+    int m;
+    cin >> m;
+    char cmd;
+    while(m--){
+        cin >> cmd;
+
+        switch (cmd) {
+            case 'L' :
+               if(t!=editor.begin()) t--;
+                break;
+            case 'D' :
+                if(t!=editor.end()) t++;
+                break;
+            case 'B' :
+               // cout << *t <<" ";
+
+                if(t!=editor.begin()) t = editor.erase(--t);
+                break;
+            case 'P' :
+                char temp;
+                cin >> temp;
+                editor.insert(t,temp);
+//if(editor.end() != t) t++;
+                break;
+        }
+    // cout << *t <<" ";
+
+    }
+
+    for(auto i: editor){
+        cout << i ;
+    }
+
+}
