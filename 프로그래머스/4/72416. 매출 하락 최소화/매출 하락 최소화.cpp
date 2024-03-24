c6fa953aac48cfbb3708def627a03c8f8c0d1062
Daso Jung <66422476+dasoya@users.noreply.github.com>
2024-03-24 13:34:33 +0900

    [level 4] Title: 매출 하락 최소화, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
+++ 프로그래머스/4/72416. 매출 하락 최소화/매출 하락 최소화.cpp
@@ -1,75 +1,66 @@
-#include <string>
-#include <vector>
+//
+// Created by dasoya on 3/23/24.
+//
+
+
+#include "bits/stdc++.h"
 
 using namespace std;
+typedef vector<vector<int>> vi;
 
 
-vector<vector<int>> dp;
+vi dp;
 
+vi makeTree(int n, vi links){
 
-vector<vector<int>> makeTree(int n, vector<vector<int>> links){
-    vector<vector<int>> tree(n,vector<int>(0));
-    
-    for(auto i:links){
+    vi tree(n,vector<int>(0));
+    for (auto i: links) {
         tree[i[0]].push_back(i[1]);
     }
     return tree;
-    
 }
 
-int fillDp(vector<vector<int>> &tree,vector<int> &sales,int node,bool flag){
-    
-    if(dp[node][flag]!=-1) return dp[node][flag];
-    
-    if(tree[node].empty()){ // 리프 노드일때
-        
-        dp[node][flag]  = flag ? sales[node-1] : 0;
-        return dp[node][flag] ;
+int fillDP(vector<int>& sales, vi& tree, int node, bool flag){
+
+    if(dp[node][flag] != -1)
+        return dp[node][flag];
+
+    if(tree[node].empty()){
+        return dp[node][flag] = flag ? sales[node-1] : 0;
     }
-    
-    dp[node][flag] = 0 ;
+
+    dp[node][flag] = 0;
     bool attend_flag = false;
     int diff = INT32_MAX;
-    for(int i =0; i< tree[node].size();i++){
+
+    for (int i = 0; i < tree[node].size(); ++i) {
         
         int next_node = tree[node][i];
-        
-        int attend = fillDp(tree,sales,next_node,true);
-        int absent = fillDp(tree,sales,next_node,false);
+        int attend = fillDP(sales,tree,next_node,true);
+        int absent = fillDP(sales,tree,next_node,false);
         
         dp[node][flag] += min(attend, absent);
         
         if(attend < absent) attend_flag = true;
         
         diff = min(diff,attend-absent);
-        
     }
+
+    if(flag) return dp[node][flag] += sales[node-1];
     
+    if(!attend_flag) return dp[node][flag] += diff;
     
-    
-    if(flag) {
-        dp[node][flag] += sales[node-1];
-        return dp[node][flag];
-    }
-    
-    if(!attend_flag){
-        dp[node][flag] += diff;
-            return dp[node][flag];
-    }
-        
     return dp[node][flag];
-    
 }
 
 int solution(vector<int> sales, vector<vector<int>> links) {
-    int answer = 0;
+
     int v = sales.size();
 
-    dp.assign(v+1,vector<int>(2,-1));
-    vector<vector<int>> tree;
-    tree = makeTree(v+1, links);
-    
-    answer = min(fillDp(tree,sales,1,true),fillDp(tree,sales,1,false));
-    
-    return answer;
+    dp.assign(v+1, vector<int>(2,-1));
+    vector<vector<int>> tree = makeTree(v+1,links);
+
+    return min(fillDP(sales,tree,1,true),fillDP(sales,tree,1,false));
+
+
 }
