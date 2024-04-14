1b6503349ca76bb068ea1d09c986406196b62305
Daso Jung <66422476+dasoya@users.noreply.github.com>
2024-04-14 14:21:44 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,31 @@
+//
+// Created by dasoya on 4/14/24.
+//
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    
+    cin >> n;
+
+    long long dp[92][2];
+
+
+    dp[1][0] = 0;
+    dp[1][1] = 1; //1
+    dp[2][0] = 1; //10
+    dp[2][1] = 0;
+
+
+
+    for (int i = 3; i <=n; ++i) { //101 100
+        dp[i][1] = dp[i-1][0];
+        dp[i][0] = dp[i-1][0]+dp[i-1][1];
+    }
+
+    cout << dp[n][0] + dp[n][1];
+    
+}
