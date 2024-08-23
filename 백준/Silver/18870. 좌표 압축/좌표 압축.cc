c63b9adef4ea1f474473753269e4e5b37988472d
Daso Jung <66422476+dasoya@users.noreply.github.com>
2024-08-23 23:14:43 +0900

    [Silver II] Title: 좌표 압축, Time: 432 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,40 +1,20 @@
-//
-// Created by dasoya on 8/17/24.
-//
-
-#include "bits/stdc++.h"
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int n;
+int x[1000005];
+vector<int> uni; // unique
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-    int n;
-    cin >> n;
-
-   vector<int> arr,uniq;
-   int x[1000005];
-
-    for (int i = 0; i < n; ++i) {
-        int temp;
-        cin >> temp;
-
-        x[i] = temp;
-        arr.push_back(temp);
-    }
-
-    sort(arr.begin(),arr.end());
-    for (int i = 0; i < n; ++i) {
-
-        if(i==0 || arr[i-1]!=arr[i]){
-            uniq.push_back(arr[i]);
-        }
-    }
-
-    for (int i = 0; i < n; ++i) {
-        //uarr[1];
-        cout << lower_bound(uniq.begin(),uniq.end(),x[i]) - uniq.begin() <<" ";
-    }
-
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> x[i];
+    uni.push_back(x[i]);
+  }
+  sort(uni.begin(), uni.end());
+  uni.erase(unique(uni.begin(), uni.end()), uni.end());
+  for(int i = 0; i < n; i++)
+    cout << lower_bound(uni.begin(), uni.end(), x[i]) - uni.begin() << ' ';
 }
