c61f522112a418c9a8e8577411e83fadfabd90e5
Daso Jung <66422476+dasoya@users.noreply.github.com>
2024-12-28 07:06:42 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -1,72 +1,27 @@
-//
-// Created by dasoya on 12/28/24.
-//
-
-
-#include "bits/stdc++.h"
-
+#include<iostream>
 using namespace std;
-
-int varr[9],warr[9];
-int maxNum = 0;
-
-void solve(int cnt,int n){
-    if(cnt == n){
-        int sum = 0;
-
-        for(int i = 0 ;i < n;i++){
-           // cout << varr[i] <<" ";;
-            if(varr[i]<=0)
-                sum++;
-        }
-       // cout << "\n";
-
-        if(maxNum < sum)
-            maxNum = sum;
-        return;
-    }
-
-    if(varr[cnt]<=0){
-        solve(cnt+1,n);
-        return;
-    }
-
-    bool flag = false;
-
-    for(int i = 0 ;i < n;i++){
-
-        if(cnt == i||varr[i] <= 0) continue;
-        flag = true;
-       // if(cnt != i)
-        { varr[i] -= warr[cnt]; varr[cnt] -= warr[i];}
-        solve(cnt+1,n);
-        //if(cnt != i)
-        { varr[i] += warr[cnt]; varr[cnt] += warr[i];}
-    }
-
-
-    if(!flag){
-        solve(cnt+1,n);
-    }
-
-
+int n,s[8],w[8],ans;
+void sol(int idx,int cnt){
+	ans=ans>cnt?ans:cnt;
+	if(idx==n)
+		return;
+	if(s[idx]<=0){
+		sol(idx+1,cnt);
+		return;
+	}
+	for(int i=0;i<n;++i){
+		if(i==idx||s[i]<=0) continue;
+		s[idx]-=w[i];
+		s[i]-=w[idx];
+		sol(idx+1,cnt+(s[idx]<=0)+(s[i]<=0));
+		s[idx]+=w[i];
+		s[i]+=w[idx];
+	}
 }
-
 int main(){
-
-
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-
-    int n;
-
-    cin >> n ;
-
-    for(int i = 0; i < n ; i++){
-        cin >> varr[i] >> warr[i];
-    }
-
-    solve(0,n);
-
-    cout << maxNum;
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	cin>>n;
+	for(int i=0;i<n;++i) cin>>s[i]>>w[i];
+	sol(0,0);
+	cout<<ans<<'\n';
 }
