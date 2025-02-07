c1ecf9fdffc8f84fd7a54a38e553e762116ddfef
Daso Jung <66422476+dasoya@users.noreply.github.com>
2025-02-07 15:33:45 +0900

    [Platinum V] Title: 비숍, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.cc
+++ 백준/Platinum/1799. 비숍/비숍.cc
@@ -1,68 +1,44 @@
-//
-// Created by dasoya on 2/6/25.
-//
-
-
-#include "bits/stdc++.h"
-
+// Authored by : heheHwang
+// Co-authored by : -
+// http://boj.kr/d3cbc0df3bc34ea0899616c42cb8f1e3
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int,int>> board[2][20];
+// 체스판의 색깔((r,c)에 대해 r+c가 짝수일 경우 0에 대응, 홀수일 경우 1에 대응)에 따라 분리해서 계산
 
-int n;
-
-int visited[2][1000];//대각선 방문 여부 (n-1)*2+1
-//int visit2[1000];
-
+vector<pair<int, int>> board[2][20]; // board[i][j] : 색깔 i에 대해 \ 방향 대각선에 포함되는 칸들을 저장하는 vector
+bool used[2][20];
 int ans[2];
-
-void back(int cnt,int idx,int color){ // cnt는 현재 비숍을 넣은 갯수.
-
-
-    if(idx>= 2*n){
-        ans[color] = max(cnt,ans[color]);
-        return;
-    }
-
-    for (auto v : board[color][idx]) {
-
-        int i = v.first;
-        int j = v.second;
-
-        if(visited[color][i+j]) continue;
-
-        visited[color][i+j] = true;
-        back(cnt+1,idx+1,color);
-        visited[color][i+j] = false;
-    }
-
-    back(cnt,idx+1,color);
-
+int N;
+void bishop(int idx, int cnt, int color) {
+  if (idx == 2 * N) {
+    ans[color] = max(ans[color], cnt);
+    return;
+  }
+  for (auto v : board[color][idx]) {
+    int x, y;
+    tie(x, y) = v;
+    if (used[color][x + y]) continue;
+    used[color][x + y] = true;
+    bishop(idx + 1, cnt + 1, color);
+    used[color][x + y] = false;
+  }
+  bishop(idx + 1, cnt, color);
 }
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-
-int main(){
-
-    cin.tie(0);
-
-    cin >> n;
-
-    int d;
-    for (int i = 0; i < n; ++i) {
-        for(int j = 0 ; j < n;j++){
-            cin >> d;
-
-            if(d){
-                board[(i+j)%2][i-j+n-1].push_back({i,j});
-            }
-        }
+  cin >> N;
+  int d;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> d;
+      if (d)
+        board[(i + j) % 2][N + i - j - 1].push_back({i, j});
     }
-
-    back(0,0,0);
-    back(0,0,1);
-
-    cout << ans[0]+ans[1];
-
-
-
+  }
+  bishop(0, 0, 0);
+  bishop(0, 0, 1);
+  cout << ans[0] + ans[1];
 }
