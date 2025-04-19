8607568eb5283bcb33e1ac116edbf60c839ab443
Daso Jung <66422476+dasoya@users.noreply.github.com>
2025-04-19 20:46:56 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    answer = my_string.substr(0,s);
+    answer += overwrite_string;
+    
+    answer += my_string.substr(s+overwrite_string.length(),
+                               my_string.length()- s+overwrite_string.length()
+                              );
+    
+    return answer;
+}
